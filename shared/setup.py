342f42693d13dc23ea4ae584d701901b57ba9d59
David Ramirez Monroy <david.ramirez@archi.com>
2024-11-17 02:45:26 -0500

    ✨ add __init__.py

--- shared/setup.py
+++ shared/setup.py
@@ -7,15 +7,17 @@ README_PATH = os.path.join(os.getcwd(), 'README.md')
 with open(README_PATH, 'r',encoding='UTF-8') as f:
     readme = f.read()
 
+VERSION = '0.0.1'
+
 setup(
     name='shared',
-    version='0.0.1',
+    version=VERSION,
     description='Shared library for Heimdall services',
     long_description=readme,
     author='Heimdall',
     packages=find_packages(
         include=[
-            '.'
+            'shared'
         ],
         exclude=[
             'test',
