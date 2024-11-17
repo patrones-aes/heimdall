5cc3133b9a7b5f8c398bef084021fbf1eaaf2377
David Ramirez Monroy <david.ramirez@archi.com>
2024-11-17 03:06:36 -0500

    ✨ update setup.py

--- shared/setup.py
+++ shared/setup.py
@@ -1,10 +1,9 @@
-import os
-
 from setuptools import setup, find_packages
+import os
 
 README_PATH = os.path.join(os.getcwd(), 'README.md')
 
-with open(README_PATH, 'r',encoding='UTF-8') as f:
+with open(README_PATH, 'r', encoding='UTF-8') as f:
     readme = f.read()
 
 VERSION = '0.0.2'
@@ -14,21 +13,15 @@ setup(
     version=VERSION,
     description='Shared library for Heimdall services',
     long_description=readme,
+    long_description_content_type='text/markdown',  # Explicit content type
     author='Heimdall',
-    packages=find_packages(
-        include=[
-            'shared'
-        ],
-        exclude=[
-            'test',
-        ]
-    ),
+    packages=find_packages(include=['shared', 'shared.*']),  # Include all submodules
     install_requires=[
         'boto3==1.35.63'
     ],
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.13',  # Correct Python version
         'Intended Audience :: Developers',
         'Operating System :: Unix'
     ]
