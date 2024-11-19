4c9f15e4f3094d459a6023789dedaa75fac2595e
David Ramirez Monroy <david.ramirez@archi.com>
2024-11-18 23:25:10 -0500

    ✨ add time constants
    add created_at and updated_at fields for every table

--- shared/modi/core/base_model.py
+++ shared/modi/core/base_model.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timezone
 from pynamodb.models import Model
+from pynamodb.attributes import UTCDateTimeAttribute
 from modi.config.settings import settings
 
 class BaseModel(Model):
@@ -10,3 +12,14 @@ class BaseModel(Model):
         aws_access_key_id = settings.AWS_ACCESS_KEY_ID
         aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
         region = settings.AWS_DEFAULT_REGION
+
+    created_at = UTCDateTimeAttribute(
+    attr_name = 'created_at',
+    default_for_new=lambda: datetime.now(timezone.utc),
+    null=False
+    )
+    updated_at = UTCDateTimeAttribute(
+        attr_name = 'updated_at',
+        default_for_new=lambda: datetime.now(timezone.utc),
+        null=False
+    )
